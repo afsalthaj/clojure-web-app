17d1ccc37b6f694f279f5dbdb883ff4bd06c4aba
Afsal Thaj <athaj@simplemachines.com.au>
2017-06-04 12:00:52 +1000

    a simple webapp using Ring

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,12 +1,19 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn welcome
+  [request]
+  (if (= "/" (:uri request)) 
+    {:status 200
+     :body "<h1>Hello, Clojure World</h1>
+            <p> Welcome to your first Clojure app.</p>"
+     :headers {}}
+    {:status 404
+     :body "<h1> This is not the page you are looking for</h1>"
+     :headers {}}))
+
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty
-     (fn [request] {:status 200
-                   :body "Hello, Clojure World"
-                   :headers {}})
-     {:port (Integer. port-number)}))
-
+  (jetty/run-jetty welcome
+                   {:port (Integer. port-number)}))
